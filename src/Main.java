5bfbe8e9d3454e02fc9df7a364f2036c59c65b01
csa21472001 <21461901as@gmail.com>
2023-05-29 11:52:16 +0300

    Мастер ветка. Финал.

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,110 @@ import java.util.concurrent.ThreadLocalRandom;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
     }
-    public static void task6()
-    {   System.out.println("Задача 6");
+
+    public static void task1() {
+        System.out.println("Задача 1");
         //int age = (int) Math.floor(Math.random());
-        int passenger = ThreadLocalRandom.current().nextInt(0,  150 + 1);
-        if (passenger>0 && passenger<=60 ) {
-            System.out.println("Вы счастливый пассажир номер "+passenger+" для вас есть сидячее место");
-        } else if (passenger>60&&passenger<=102){
-            System.out.println("Вы пассажир номер "+passenger+" для вас есть стоячее место");
-        } else {
-            System.out.println("Мест нет. Пожалуйста покиньте вагон");
+        int clientOS = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+        //int clientDeviceYear = ThreadLocalRandom.current().nextInt(2007,  2023 + 1);
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для IOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
+
+    public static void task2()
+    {   System.out.println("Задача 2");
+        int clientOS = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+        int clientDeviceYear = ThreadLocalRandom.current().nextInt(2007, 2023 + 1);
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+    public static void task3()
+    {   System.out.println("Задача 3");
+        int year = ThreadLocalRandom.current().nextInt(4, 200 + 1);
+        if (year%4==0){
+            if ((year%100)==0) {
+                System.out.println(year + " год не является високосным");
+            } else if ((year%400)==0) {
+                System.out.println(year + " год является високосным");
+            } else { System.out.println(year + " год является високосным");}
+            } else {   System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void task4()
+    {   System.out.println("Задача 4");
+        int deliveryDistance = ThreadLocalRandom.current().nextInt(1, 20 + 1);
+        if (deliveryDistance<=20){
+            System.out.println("Доставка потребует 1 день ");
+        } else if (deliveryDistance>20&&deliveryDistance<=60) {
+            System.out.println("Доставка потребует 2-ое суток");
+        }  else if (deliveryDistance>60&&deliveryDistance<=100) {
+            System.out.println("Доставка потребует 3-ое суток");
+        }   else {
+            System.out.println("Свыше 100 км доставки нет.");}
+    }
+    public static void task5()
+    {   System.out.println("Задача 5");
+        int monthNumber = ThreadLocalRandom.current().nextInt(1, 20 + 1);
+        switch(monthNumber) {
+            case 1:
+                System.out.println("Месяц Январь. Зима.");
+                break;
+            case 2:
+                System.out.println("Месяц Февраль. Зима.");
+                break;
+            case 3:
+                System.out.println("Месяц Март. Весна.");
+                break;
+            case 4:
+                System.out.println("Месяц Апрель. Весна.");
+                break;
+            case 5:
+                System.out.println("Месяц Май. Весна.");
+                break;
+            case 6:
+                System.out.println("Месяц Июнь. Лето.");
+                break;
+            case 7:
+                System.out.println("Месяц Июль. Лето.");
+                break;
+            case 8:
+                System.out.println("Месяц Август. Лето.");
+                break;
+            case 9:
+                System.out.println("Месяц Сентябрь. Осень.");
+                break;
+            case 10:
+                System.out.println("Месяц Октябрь. Осень.");
+                break;
+            case 11:
+                System.out.println("Месяц Ноябрь. Осень.");
+                break;
+            case 12:
+                System.out.println("Месяц Декабрь. Зима.");
+                break;
+            default:
+                System.out.println("Ошибка. Нет такого месяца.");
+                break;
+        }
+    }
+
 }
+
